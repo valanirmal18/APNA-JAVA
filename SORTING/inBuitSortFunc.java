1c970a44dd0bcbb5c6e172ad082379dd4fb4de00
valanirmal18 <nirmalvala18@gmail.com>
2024-02-10 14:26:46 +0530

    commit

--- SORTING/inBuitSortFunc.java
+++ SORTING/inBuitSortFunc.java
@@ -7,9 +7,19 @@ public class inBuitSortFunc {
         }
         System.out.println();
     }
+    public static void prirntArr(Integer arr[]) {
+        for (Integer i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         int arr[] = {4,1,8,6,45,23};
+        Integer arr2[] = {4,1,8,6,45,23};
         Arrays.sort(arr);
         prirntArr(arr);
+        Arrays.sort(arr2, Collections.reverseOrder());
+        prirntArr(arr2);
     }
 }
+
